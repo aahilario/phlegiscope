bab1c3c28ab7040c92ab4680c1a6957abdab2433
avahilario@gmail.com <avahilario@gmail.com>
2018-07-28 23:37:01 +0800

    Add container for Constitution section variant (anchor record for comparable sections / lines)

---
+++ system/lib/models/model.variant.article.constitution.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Class ConstitutionArticleVariantModel
+ * Legiscope - web site reflection framework
+ *
+ * Antonio A Hilario
+ * Release license terms: GNU Public License V2
+ * Generated on Sat, 28 Jul 2018 08:40:42 +0000
+ */
+
+class ConstitutionArticleVariantModel extends DatabaseUtility {
+  
+  function __construct() {
+    parent::__construct();
+  }
+
+}
+

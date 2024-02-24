b71cbeeb65943fcfb57075e4f9994147fb1c869c
Antonio A Hilario <aahilario@gmail.com>
2024-02-24 15:09:01 +0800

    Add dev tools directory

---
+++ legiscope.nodejs/tools/create-visited-json.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -f r.json
+for U in $(cat q.json | jq -r .[] | sed -r -e 's@/../@/@g' | sort -u); do
+  cat <<EOF >> r.json
+"$U",
+EOF
+  cat <<EOF
+  $U: {
+    "hits": 1
+  }
+EOF
+done
+

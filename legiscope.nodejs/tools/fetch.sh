558fe5206165f883aa2b1ed43b1edbde064f42ad
Antonio A Hilario <aahilario@gmail.com>
2024-02-26 01:43:12 +0800

    Add command-line shell script

---
+++ legiscope.nodejs/tools/fetch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -f output.log
+#export SILENT_PARSE=1 
+export RECURSIVE=1 
+export REFRESH=1
+export TARGETURL="$1" 
+time npx wdio run ./wdio.conf.js 2>&1 | tee -a output.log
+cat lastrun.json | jq .
+

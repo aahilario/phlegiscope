d453d9cb560959674d7927eb4ad06d8401050f6b
Antonio A Hilario <aahilario@gmail.com>
2024-02-26 11:52:28 +0800

    Checkpoint: Fixed URL normalization

--- legiscope.nodejs/tools/fetch.sh
+++ legiscope.nodejs/tools/fetch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rm -f output.log
-export SILENT_PARSE=1 
+#export SILENT_PARSE=1 
 export RECURSIVE=1 
-#export REFRESH=1
+export REFRESH=1
 export TARGETURL="$1" 
 time npx wdio run ./wdio.conf.js 2>&1 | tee -a output.log
 cat lastrun.json | jq .

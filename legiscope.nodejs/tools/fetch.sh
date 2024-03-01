6ea22e79a3c5e4f7337db3de54a8ce4f52da56bb
Antonio A Hilario <aahilario@gmail.com>
2024-03-01 23:45:30 +0800

    Checkpoint fetch 2023-03-01 congress.gov.ph

--- legiscope.nodejs/tools/fetch.sh
+++ legiscope.nodejs/tools/fetch.sh
@@ -4,7 +4,7 @@ rm -f output.log
 export SILENT_PARSE=1 
 #export NOISY_PARSE=1
 export RECURSIVE=1 
-export REFRESH=1
+#export REFRESH=1
 #export SKIP_HEAD_FETCH=1
 export TARGETURL="$1" 
 # time npx --expose-gc wdio run ./wdio.conf.js 2>&1 | tee -a output.log
@@ -14,5 +14,5 @@ time node \
   --max-old-space-size=8192 \
   --max-heap-size=8192 \
   --huge-max-old-generation-size \
-  ./node_modules/.bin/wdio run ./wdio.conf.js | tee -a output.log
+  ./node_modules/.bin/wdio run ./wdio.conf.js | tee -a output.log | less
 cat lastrun.json | jq .  | tee -a output.log

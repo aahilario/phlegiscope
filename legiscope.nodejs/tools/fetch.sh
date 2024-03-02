049118db5aa9d6c1c013e0609e0f4e44c8bf919a
Antonio A Hilario <aahilario@gmail.com>
2024-03-02 18:11:36 +0800

    Clean up fetch test directory congress.gov.ph

--- legiscope.nodejs/tools/fetch.sh
+++ legiscope.nodejs/tools/fetch.sh
@@ -3,17 +3,18 @@
 rm -f output.log
 export SILENT_PARSE=1 
 #export NOISY_PARSE=1
-#export RECURSIVE=1 
+export RECURSIVE=1 
 export REFRESH=1
 #export SKIP_HEAD_FETCH=1
 export TARGETURL="$1" 
 export SHOW_HEADINFO=1
 # time npx --expose-gc wdio run ./wdio.conf.js 2>&1 | tee -a output.log
 time node \
-  --expose-gc \
-  --trace-gc \
   --max-old-space-size=8192 \
   --max-heap-size=8192 \
   --huge-max-old-generation-size \
   ./node_modules/.bin/wdio run ./wdio.conf.js | tee -a output.log
+  #--expose-gc \
+  #--trace-gc \
+
 cat lastrun.json | jq .  | tee -a output.log

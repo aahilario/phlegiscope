9a89175fc689760f7823df521853179b419e8d24
Antonio A Hilario <aahilario@gmail.com>
2024-03-08 03:03:12 +0800

    Checkpoint: Updating path handling

--- legiscope.nodejs/tools/fetch.sh
+++ legiscope.nodejs/tools/fetch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 rm -f output.log
-#export SILENT_PARSE=1 
+export SILENT_PARSE=1 
 export NOISY_PARSE=1
-export RECURSIVE=1 
+#export RECURSIVE=1 
 export REFRESH=1
 #export SKIP_HEAD_FETCH=1
+export FORCE_INTERACTABLE_HEAD_FETCH=1
 export TARGETURL="$1" 
 export SHOW_HEADINFO=1
+export FORCE_EXTRACT=1
 # time npx --expose-gc wdio run ./wdio.conf.js 2>&1 | tee -a output.log
 time node \
   --max-old-space-size=8192 \
